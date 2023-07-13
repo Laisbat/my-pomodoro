922367422d8bd6478903517374dde8ccc4643d23
Lais <laisbatistapereira@gmail.com>
2023-07-12 23:43:03 -0300

    :sparkles: Implement DefaultLayout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import { History } from './pages/History'
 import { Home } from './pages/Home'
-import { Default } from './layouts/Default'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Default />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
